d45b73b5d6c11eae9d32a02c42182584974ac3b3
Goekalp <Goekalpyildiz@outlook.de>
2023-04-02 18:43:36 +0200

    test

---
+++ seed.js
@@ -0,0 +1,42 @@
+const submissions = [
+  {
+    id: 1,
+    title: "Spaghetti Bolognese",
+    desc: "Ein Nudelgericht mit Hackfleischsoße.",
+    votes: 16,
+    author: "Italien",
+    img: "https://via.placeholder.com/64",
+  },
+  {
+    id: 2,
+    title: "Wiener Schnitzel",
+    desc: "Ein dünnes Schnitzel aus Kalbfleisch.",
+    votes: 5,
+    author: "Österreich",
+    img: "https://via.placeholder.com/64",
+  },
+  {
+    id: 3,
+    title: "Peking-Ente",
+    desc: "Das kaiserliche Gericht.",
+    votes: 20,
+    author: "China",
+    img: "https://via.placeholder.com/64",
+  },
+  {
+    id: 4,
+    title: "Gulasch",
+    desc: "Ein traditionelles Ragout.",
+    votes: 24,
+    author: "Ungarn",
+    img: "https://via.placeholder.com/64",
+  },
+  {
+    id: 5,
+    title: "Tavuk",
+    desc: "Ein traditionelles Hähnchen.",
+    votes: 35,
+    author: "Türkei",
+    img: "https://via.placeholder.com/64",
+  },
+];
