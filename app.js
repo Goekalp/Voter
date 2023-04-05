abf1dd770c0d203abf35169055208907958fed0b
Goekalp <Goekalpyildiz@outlook.de>
2023-04-05 19:12:18 +0200

    Event handling with argument

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
       submissions: submissions, //aus seed.js
     };
   },
+  methods: {
+    /* upvote: function () {}, */
+    upvote(infoText, event) {
+      //console.log(this);
+      this.submissions[0].votes++;
+      console.log(infoText);
+      console.log(event);
+    },
+  },
 });
 
 //Liefert die Instanz zur Root-Component zurück
